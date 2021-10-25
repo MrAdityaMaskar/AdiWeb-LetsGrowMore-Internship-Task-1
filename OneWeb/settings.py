61df86abe051dff23b1d97509413a61f9dce498f
Aditya Maskar <aditya01092001@gmail.com>
2021-10-25 22:26:39 +0530

    second commit

--- OneWeb/settings.py
+++ OneWeb/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'OneWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates', 'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
